c3501823a69969401c37247b9cc7793900a545f9
Marco Haber <marcoshaber99@gmail.com>
2022-06-20 16:19:20 +0300

    testing size of bg image

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+  const getKey = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+  if (!audio) return; //stop function from running completely
 
-
-function playSound (e) {
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    const getKey = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
-    if (!audio) return;//stop function from running completely
-
-    /*in order to be able to hit it multiple times while it is still "playing"
+  /*in order to be able to hit it multiple times while it is still "playing"
     we need to rewind it every time it is being pressed */
 
-    audio.currentTime=0; //rewind to the start
-    audio.play();
-    getKey.classList.add('playing');
-
+  audio.currentTime = 0; //rewind to the start
+  audio.play();
+  getKey.classList.add("playing");
 }
-function removeTransition(e) { 
-    if(e.propertyName !== 'transform') 
-    { 
-        return; //do nothing if it not a tranform
-    }
-    this.classList.remove('playing');
+function removeTransition(e) {
+  if (e.propertyName !== "transform") {
+    return; //do nothing if it not a tranform
+  }
+  this.classList.remove("playing");
 }
 
-const getAllKeys = document.querySelectorAll('.key');
-getAllKeys.forEach(getKey=>getKey.addEventListener('transitionend', removeTransition));
+const getAllKeys = document.querySelectorAll(".key");
+getAllKeys.forEach((getKey) =>
+  getKey.addEventListener("transitionend", removeTransition)
+);
 
-window.addEventListener('keydown',playSound);
-
+window.addEventListener("keydown", playSound);
